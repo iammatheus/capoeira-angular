382f1f11353fb6facdf4915fb4a309fe5cdafa83
Matheus Ferreira Siqueira <matheusferreirasiqueira9@gmail.com>
2022-10-21 00:21:00 -0300

    alterações

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-let express = require('express');
-let path = require('path');
-let app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.use(express.static(__dirname+'/dist/capoeira-angular'))
+app.use(express.static(__dirname+'/dist/frontend'))
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/dist/capoeira-angular/index.html'))
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname+'/dist/frontend/index.html'))
 });
 
 app.listen(process.env.PORT || 8080);
